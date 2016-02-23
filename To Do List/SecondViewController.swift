a52637ad8a13ae4f8022cc20fbc12e5a267516e7
Eduardo Alvarado <e.alvaradod@gmail.com>
2016-02-23 00:29:05 -0600

    first commit

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet var item: UITextField!
 
+    @IBAction func addItem(sender: AnyObject) {
+        toDoList.append(item.text!)
+        item.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,14 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        item.resignFirstResponder()
+        return true
+    }
 
 }
 
